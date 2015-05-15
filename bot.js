cfe94f3245609489818e18250be796351fec9070
mxk295 <malavika.kesavan@gmail.com>
2015-05-15 12:55:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool[randomIntInc(1,17)];
+  botResponse = cool[random.cool.length()];
 
   options = {
     hostname: 'api.groupme.com',
